91a610e0374ce929f4a6baa1a8384765f68881c8
fl4mek1ng <106778185+fl4mek1ng@users.noreply.github.com>
2022-08-24 01:29:40 -0600

    Add files via upload

---
+++ Pokeapi/Pokeapi/src/test/java/com/PokeAPI/Pokeapi/PokeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PokeAPI.Pokeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
